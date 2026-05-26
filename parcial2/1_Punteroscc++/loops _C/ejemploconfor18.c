5e06b984069711bdd16f4427ceb8a2b001291747
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-05-26 16:51:19 -0500

    Create ejemploconfor18.c

---
+++ parcial2/1_Punteroscc++/loops _C/ejemploconfor18.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int numero = 7; //numero del que se desea la tabla
+    int  i;// contador
+    
+    for( i = 1;  i <=10;  i++){
+        printf("%d x %d = %d\n", numero,  i, numero *  i);
+    }
+    
+return 0;
+}
