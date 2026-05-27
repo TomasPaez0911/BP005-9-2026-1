721823dfef9d8f0d7f004f46cd5e3664643241db
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-05-27 04:44:57 -0500

    Update tallerconversortemperatura.cpp

--- parcial2/1_Punteroscc++/funcionescyc++/tallerconversortemperatura.cpp
+++ parcial2/1_Punteroscc++/funcionescyc++/tallerconversortemperatura.cpp
@@ -1,29 +1,20 @@
 #include <iostream>
 using namespace std;
-
-/* PROTOTIPOS */
 float convertirAFahrenheit(float celsius);
-
 int main() {
-
-    /* DECLARACION DE VARIABLES */
     float celsius;
     float fahrenheit;
 
-    /* ENTRADA */
     cout << "Digite la temperatura en Celsius: ";
     cin >> celsius;
 
-    /* PROCESO */
+  
     fahrenheit = convertirAFahrenheit(celsius);
 
-    /* SALIDA */
     cout << "Temperatura en Fahrenheit: " << fahrenheit << endl;
-
-    return 0;
+   return 0;
 }
 
-/* FUNCIONES */
 float convertirAFahrenheit(float celsius) {
     return (9.0/5.0) * celsius + 32;
 }
