2590f3c09434f04faf143ed9c140539c55abda3f
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-05-26 22:53:11 -0500

    Create ejemplo1pag11.cpp

---
+++ parcial2/1_Punteroscc++/funcionescyc++/ejemplo1pag11.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+// Prototipo de la función
+void saludar();
+
+int main() {
+    
+    saludar();
+    
+    return 0;
+}
+
+// Definición de la función
+void saludar() {
+    cout << "Hola. Estamos estudiando funciones." << endl;
+}
