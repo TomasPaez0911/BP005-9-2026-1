8b69302f4772a645f438e09d747c2110c2c471f5
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-05-27 03:59:46 -0500

    Update ejemplo_04_dos_usos_asterisco.cpp

--- parcial2/1_Punteroscc++/02_punteros_en_c.pdf/ejemplo_04_dos_usos_asterisco.cpp
+++ parcial2/1_Punteroscc++/02_punteros_en_c.pdf/ejemplo_04_dos_usos_asterisco.cpp
@@ -1,13 +1,8 @@
-#include <iostream> /* Incluye la biblioteca necesaria para std::cout y std::endl. */
-
-int main() { /* Función principal del programa. */
-    int x = 5;   /* Variable entera con valor inicial 5. */
-    int *p = &x; /* Declaración de puntero: aquí el * forma parte de la declaración. */
-
-    *p = 40;     /* Desreferenciación: aquí el * accede al contenido apuntado por p. */
-
-    // std::cout reemplaza a printf. Usamos el operador << para enviar el texto y la variable.
-    std::cout << "x = " << x << std::endl; /* Muestra el valor final de x. */
-
-    return 0; /* Finalización exitosa. */
+#include <iostream> 
+int main() { 
+    int x = 5;   
+    int *p = &x; 
+ *p = 40;     
+   std::cout << "x = " << x << std::endl; 
+    return 0;
 }
