20470aa88b7eadc7fc28b9695b21dcbdf2af0d03
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-05-26 23:05:17 -0500

    Create ejemplo5pag23.c

---
+++ parcial2/1_Punteroscc++/funcionescyc++/ejemplo5pag23.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// Prototipo de la función
+int potencia(int base, int exponente);
+
+int main() {
+    int base = 2;
+    int exponente = 4;
+    int resultado;
+
+    resultado = potencia(base, exponente);
+    printf("%d elevado a %d es %d\n", base, exponente, resultado);
+
+    return 0;
+}
+
+// Definición de la función
+int potencia(int base, int exponente) {
+    int resultado = 1;
+    int i;
+
+    for (i = 0; i < exponente; i++) {
+        resultado = resultado * base;
+    }
+
+    return resultado;
+}
