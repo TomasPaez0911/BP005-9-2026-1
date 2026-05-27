ac14829f38059b3278c188bde7995e88f0e699bf
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-05-26 21:40:50 -0500

    Create cambiovariable06.c

---
+++ parcial2/1_Punteroscc++/fotopunteroscc++/cambiovariable06.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void cambiarSinPuntero(int n) {
+    n = 100; 
+}
+
+void cambiarConPuntero(int *p) {
+    if (p != NULL) {
+        *p = 100; 
+    }
+}
+
+int main() {
+    int x = 5;
+
+  
+    printf("Antes de cambiarSinPuntero: x = %d\n", x);
+    cambiarSinPuntero(x);
+    printf("Despues de cambiarSinPuntero: x = %d\n", x); 
+    
+
+    printf("\n"); 
+
+   
+    printf("Antes de cambiarConPuntero: x = %d\n", x);
+    cambiarConPuntero(&x); 
+    printf("Despues de cambiarConPuntero: x = %d\n", x);
+    
+
+    return 0;
+}
