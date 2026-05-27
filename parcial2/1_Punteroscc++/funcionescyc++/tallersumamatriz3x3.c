ef0f2a80cf8c89d5608f85120c4a2c3b60b248c0
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-05-27 00:13:55 -0500

    Create tallersumamatriz3x3.c

---
+++ parcial2/1_Punteroscc++/funcionescyc++/tallersumamatriz3x3.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+/* PROTOTIPOS */
+void leerMatriz(int m[][3], int filas);
+void mostrarMatriz(int m[][3], int filas);
+int sumarMatriz(int m[][3], int filas);
+
+int main() {
+
+    int m[3][3];
+    int suma;
+
+    leerMatriz(m, 3);
+    mostrarMatriz(m, 3);
+
+    suma = sumarMatriz(m, 3);
+
+    printf("Suma total: %d\n", suma);
+
+    return 0;
+}
+
+/* FUNCIONES */
+void leerMatriz(int m[][3], int filas) {
+    for(int i = 0; i < filas; i++) {
+        for(int j = 0; j < 3; j++) {
+            printf("Digite m[%d][%d]: ", i, j);
+            scanf("%d", &m[i][j]);
+        }
+    }
+}
+
+void mostrarMatriz(int m[][3], int filas) {
+    printf("Matriz:\n");
+    for(int i = 0; i < filas; i++) {
+        for(int j = 0; j < 3; j++) {
+            printf("%d ", m[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int sumarMatriz(int m[][3], int filas) {
+    int suma = 0;
+    for(int i = 0; i < filas; i++) {
+        for(int j = 0; j < 3; j++) {
+            suma += m[i][j];
+        }
+    }
+    return suma;
+}
