2af9a0600fb324685d7e5c7dbb2ee0343a50e633
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-05-27 04:47:37 -0500

    Update tallerpotenciaentera.c

--- parcial2/1_Punteroscc++/funcionescyc++/tallerpotenciaentera.c
+++ parcial2/1_Punteroscc++/funcionescyc++/tallerpotenciaentera.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 
-/* PROTOTIPOS */
+
 int potencia(int base, int exponente);
 
 int main() {
 
-    /* VARIABLES */
+    
     int base, exponente, resultado;
 
-    /* ENTRADA */
+  
     printf("Digite la base: ");
     scanf("%d", &base);
 
     printf("Digite el exponente: ");
     scanf("%d", &exponente);
 
-    /* PROCESO */
+    
     resultado = potencia(base, exponente);
 
-    /* SALIDA */
+    
     printf("%d elevado a %d es %d\n", base, exponente, resultado);
 
     return 0;
 }
 
-/* FUNCIONES */
+
 int potencia(int base, int exponente) {
     int resultado = 1;
 
