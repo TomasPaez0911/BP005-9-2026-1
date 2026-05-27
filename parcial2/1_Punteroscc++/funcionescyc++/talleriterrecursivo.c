dafb81330871cb489ad4bb8199a23647b8fee07d
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-05-27 00:20:14 -0500

    Create talleriterrecursivo.c

---
+++ parcial2/1_Punteroscc++/funcionescyc++/talleriterrecursivo.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/* PROTOTIPOS */
+int factorialIterativo(int n);
+int factorialRecursivo(int n);
+
+int main() {
+
+    int n;
+
+    printf("Digite un numero: ");
+    scanf("%d", &n);
+
+    printf("Factorial iterativo: %d\n", factorialIterativo(n));
+    printf("Factorial recursivo: %d\n", factorialRecursivo(n));
+
+    return 0;
+}
+
+/* FUNCIONES */
+int factorialIterativo(int n) {
+    int r = 1;
+    for(int i = 1; i <= n; i++) {
+        r *= i;
+    }
+    return r;
+}
+
+int factorialRecursivo(int n) {
+    if(n == 0)
+        return 1;
+    else
+        return n * factorialRecursivo(n - 1);
+}
