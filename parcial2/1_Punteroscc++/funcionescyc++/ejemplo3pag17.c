02a914ddc968ac4a93b6764708b696cc9117352a
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-05-26 22:57:48 -0500

    Create ejemplo3pag17.c

---
+++ parcial2/1_Punteroscc++/funcionescyc++/ejemplo3pag17.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Prototipo de la función
+float calcularPromedio(float n1, float n2, float n3);
+
+int main() {
+    float nota1 = 4.2;
+    float nota2 = 3.8;
+    float nota3 = 4.5;
+    float promedio;
+
+    promedio = calcularPromedio(nota1, nota2, nota3);
+    printf("El promedio es: %.2f\n", promedio);
+
+    return 0;
+}
+
+// Definición de la función
+float calcularPromedio(float n1, float n2, float n3) {
+    float promedio;
+    promedio = (n1 + n2 + n3) / 3.0;
+    
+    return promedio;
+}
