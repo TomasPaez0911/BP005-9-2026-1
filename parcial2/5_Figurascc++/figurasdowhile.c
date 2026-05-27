6a6c6901b70087fa635109fa22713bf2364fee91
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-05-26 20:49:24 -0500

    Create figurasdowhile.c

---
+++ parcial2/5_Figurascc++/figurasdowhile.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void figura7() {
+    int fila = 1;
+    int columna;
+
+    do {
+        columna = 1;
+
+        do {
+            printf("= ");
+            columna++;
+        } while (columna <= 6);
+
+        printf("\n");
+        fila++;
+    } while (fila <= 3);
+}
+
+void figura8() {
+    int fila = 1;
+    int columna;
+
+    do {
+        columna = 1;
+
+        do {
+            printf("$ ");
+            columna++;
+        } while (columna <= fila);
+
+        printf("\n");
+        fila++;
+    } while (fila <= 5);
+}
+
+int main() {
+    figura7();
+    printf("\n");
+
+    figura8();
+
+    return 0;
+}
