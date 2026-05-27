5a5b68a4f12dc22224ac548d0e8a970c10910345
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-05-26 23:11:02 -0500

    Create ejemplo6pag31.c

---
+++ parcial2/1_Punteroscc++/funcionescyc++/ejemplo6pag31.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// Prototipo de la función
+int sumarArreglo(int v[], int n);
+
+int main() {
+    int datos[5] = {4, 7, 2, 9, 5};
+    int suma;
+
+    suma = sumarArreglo(datos, 5);
+    printf("La suma del arreglo es: %d\n", suma);
+
+    return 0;
+}
+
+// Definición de la función
+int sumarArreglo(int v[], int n) {
+    int suma = 0;
+    int i;
+
+    for (i = 0; i < n; i++) {
+        suma = suma + v[i];
+    }
+
+    return suma;
+}
