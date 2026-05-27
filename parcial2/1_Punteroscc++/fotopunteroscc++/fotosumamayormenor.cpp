38e494a2469e0ef023a683b94c4e0e86e0fb3ffc
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-05-27 04:28:54 -0500

    Update fotosumamayormenor.cpp

--- parcial2/1_Punteroscc++/fotopunteroscc++/fotosumamayormenor.cpp
+++ parcial2/1_Punteroscc++/fotopunteroscc++/fotosumamayormenor.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
-
-// Usamos referencias (&) en lugar de punteros (*)
 void analizar(int v[], int n, int &suma, int &mayor, int &menor) {
     suma = 0;
     mayor = v[0];
     menor = v[0];
 
-    for (int i = 0; i < n; i++) {
+for (int i = 0; i < n; i++) {
         suma = suma + v[i];
 
-        if (v[i] > mayor) {
+if (v[i] > mayor) {
             mayor = v[i];
         }
-
-        if (v[i] < menor) {
+if (v[i] < menor) {
             menor = v[i];
         }
     }
@@ -22,10 +19,7 @@ void analizar(int v[], int n, int &suma, int &mayor, int &menor) {
 int main() {
     int datos[5] = {8, 3, 15, 6, 10};
     int suma, mayor, menor;
-
-    // Ya no es necesario anteponer el '&' a las variables
-    analizar(datos, 5, suma, mayor, menor);
-
+analizar(datos, 5, suma, mayor, menor);
     std::cout << "Suma  = " << suma << "\n";
     std::cout << "Mayor = " << mayor << "\n";
     std::cout << "Menor = " << menor << "\n";
