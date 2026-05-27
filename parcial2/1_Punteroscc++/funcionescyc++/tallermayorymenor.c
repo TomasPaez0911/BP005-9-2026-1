5aa900de205b9bd1a28ca28eb224f6ee4040c234
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-05-27 00:11:23 -0500

    Create tallermayorymenor.c

---
+++ parcial2/1_Punteroscc++/funcionescyc++/tallermayorymenor.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/* PROTOTIPOS */
+void leerArreglo(int v[], int n);
+int obtenerMayor(int v[], int n);
+int obtenerMenor(int v[], int n);
+
+int main() {
+
+    int v[6];
+    int mayor, menor;
+
+    leerArreglo(v, 6);
+
+    mayor = obtenerMayor(v, 6);
+    menor = obtenerMenor(v, 6);
+
+    printf("Mayor: %d\n", mayor);
+    printf("Menor: %d\n", menor);
+
+    return 0;
+}
+
+/* FUNCIONES */
+void leerArreglo(int v[], int n) {
+    for(int i = 0; i < n; i++) {
+        printf("Digite el elemento %d: ", i);
+        scanf("%d", &v[i]);
+    }
+}
+
+int obtenerMayor(int v[], int n) {
+    int mayor = v[0];
+    for(int i = 1; i < n; i++) {
+        if(v[i] > mayor)
+            mayor = v[i];
+    }
+    return mayor;
+}
+
+int obtenerMenor(int v[], int n) {
+    int menor = v[0];
+    for(int i = 1; i < n; i++) {
+        if(v[i] < menor)
+            menor = v[i];
+    }
+    return menor;
+}
