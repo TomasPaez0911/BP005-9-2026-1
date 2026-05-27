c22b89d555ee6c33a70577b48f9a01a1535bc366
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-05-27 04:41:08 -0500

    Update ejemplo7pag34.cpp

--- parcial2/1_Punteroscc++/funcionescyc++/ejemplo7pag34.cpp
+++ parcial2/1_Punteroscc++/funcionescyc++/ejemplo7pag34.cpp
@@ -1,31 +1,24 @@
 #include <iostream>
-
 using namespace std;
 
 // Prototipo de la función
 int sumarMatriz(int m[][3], int filas);
-
 int main() {
-    // Declaración de la matriz en forma de cuadrícula de 3x3
+    
     int matriz[3][3] = {
         {1, 2, 3},
         {4, 5, 6},
         {7, 8, 9}
     };
-    
-    int suma;
+     int suma;
     suma = sumarMatriz(matriz, 3);
-    
     cout << "La suma de la matriz es: " << suma << endl;
-    
-    return 0;
+     return 0;
 }
 
-// Definición de la función
+
 int sumarMatriz(int m[][3], int filas) {
     int suma = 0;
-    
-    // Ciclos anidados para recorrer filas (i) y columnas (j)
     for (int i = 0; i < filas; i++) {
         for (int j = 0; j < 3; j++) {
             suma = suma + m[i][j];
