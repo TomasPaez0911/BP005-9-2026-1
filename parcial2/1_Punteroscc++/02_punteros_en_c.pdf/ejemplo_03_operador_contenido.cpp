553e4299d7ad329b1b59aa630c06fbe60da6985c
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-05-27 03:58:23 -0500

    Update ejemplo_03_operador_contenido.cpp

--- parcial2/1_Punteroscc++/02_punteros_en_c.pdf/ejemplo_03_operador_contenido.cpp
+++ parcial2/1_Punteroscc++/02_punteros_en_c.pdf/ejemplo_03_operador_contenido.cpp
@@ -1,16 +1,12 @@
-#include <iostream> /* Incluye la biblioteca necesaria para std::cout y std::endl. */
+#include <iostream> 
+int main() { 
+    int x = 25;  
+    int *p = &x; 
+    std::cout << "x = " << x << std::endl;  
+    std::cout << "*p = " << *p << std::endl; 
 
-int main() { /* Función principal del programa. */
-    int x = 25;  /* Variable entera con valor inicial 25. */
-    int *p = &x; /* p guarda la dirección de x. */
+    *p = 99; 
 
-    // std::cout reemplaza a printf. Usamos << para encadenar las variables y el texto.
-    std::cout << "x = " << x << std::endl;   /* Muestra el valor directo de x. */
-    std::cout << "*p = " << *p << std::endl; /* Muestra el valor encontrado en la dirección guardada en p. */
-
-    *p = 99; /* Modifica x indirectamente mediante el puntero p. */
-
-    std::cout << "x despues = " << x << std::endl; /* Muestra x después de la modificación indirecta. */
-
-    return 0; /* Finalización exitosa. */
+    std::cout << "x despues = " << x << std::endl; 
+ return 0; 
 }
