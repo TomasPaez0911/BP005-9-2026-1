b0b33c29d4356fa9bf13c3ba9e889628e48e6b13
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-05-27 03:57:29 -0500

    Update ejemplo_03_operador_contenido.c

--- parcial2/1_Punteroscc++/02_punteros_en_c.pdf/ejemplo_03_operador_contenido.c
+++ parcial2/1_Punteroscc++/02_punteros_en_c.pdf/ejemplo_03_operador_contenido.c
@@ -1,15 +1,10 @@
-#include <stdio.h> /* Incluye la biblioteca necesaria para printf. */
-
-int main(void) { /* Función principal del programa. */
-    int x = 25;  /* Variable entera con valor inicial 25. */
-    int *p = &x; /* p guarda la dirección de x. */
-
-    printf("x = %d\n", x);   /* Muestra el valor directo de x. */
-    printf("*p = %d\n", *p); /* Muestra el valor encontrado en la dirección guardada en p. */
-
-    *p = 99; /* Modifica x indirectamente mediante el puntero p. */
-
-    printf("x despues = %d\n", x); /* Muestra x después de la modificación indirecta. */
-
-    return 0; /* Finalización exitosa. */
+#include <stdio.h> 
+int main(void) { 
+    int x = 25;  
+    int *p = &x; 
+    printf("x = %d\n", x);   
+    printf("*p = %d\n", *p); 
+    *p = 99; 
+    printf("x despues = %d\n", x);
+return 0; 
 }
