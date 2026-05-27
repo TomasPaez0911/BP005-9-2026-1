ea81434788ab3134a46ee4300e54f252b80e69fa
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-05-27 04:44:18 -0500

    Update tallerconversortemperatura.c

--- parcial2/1_Punteroscc++/funcionescyc++/tallerconversortemperatura.c
+++ parcial2/1_Punteroscc++/funcionescyc++/tallerconversortemperatura.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
-/* PROTOTIPOS */
+
 float convertirAFahrenheit(float celsius);
 
 int main() {
 
-    /* DECLARACION DE VARIABLES */
+   
     float celsius;
     float fahrenheit;
 
-    /* ENTRADA DE DATOS */
+ 
     printf("Digite la temperatura en Celsius: ");
     scanf("%f", &celsius);
 
-    /* PROCESO */
+   
     fahrenheit = convertirAFahrenheit(celsius);
 
-    /* SALIDA */
+ 
     printf("Temperatura en Fahrenheit: %.2f\n", fahrenheit);
 
     return 0;
 }
 
-/* DEFINICION DE FUNCIONES */
+
 float convertirAFahrenheit(float celsius) {
     return (9.0/5.0) * celsius + 32;
 }
