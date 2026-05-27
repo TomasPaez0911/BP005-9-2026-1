a58cbbb25bb5ceb2a9776a804638b3ef2db5e170
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-05-26 22:52:13 -0500

    Create ejemplo1pag11.c

---
+++ parcial2/1_Punteroscc++/funcionescyc++/ejemplo1pag11.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Prototipo de la función
+void saludar(void);
+
+int main() {
+    
+    saludar();
+    
+    return 0;
+}
+
+// Definición de la función
+void saludar(void) {
+    printf("Hola. Estamos estudiando funciones.\n");
+}
