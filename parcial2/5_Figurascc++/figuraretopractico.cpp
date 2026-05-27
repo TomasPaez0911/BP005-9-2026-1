d28a850044254767fb80102daae7cf9eddb5dd3c
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-05-26 21:01:30 -0500

    Create figuraretopractico.cpp

---
+++ parcial2/5_Figurascc++/figuraretopractico.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Ingrese el tamano del rombo: ";
+    cin >> n;
+
+    // Parte superior del rombo
+    for (int fila = 1; fila <= n; fila++) {
+        for (int espacios = 1; espacios <= n - fila; espacios++) {
+            cout << " ";
+        }
+        for (int simbolos = 1; simbolos <= 2 * fila - 1; simbolos++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    // Parte inferior del rombo
+    for (int fila = n - 1; fila >= 1; fila--) {
+        for (int espacios = 1; espacios <= n - fila; espacios++) {
+            cout << " ";
+        }
+        for (int simbolos = 1; simbolos <= 2 * fila - 1; simbolos++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
