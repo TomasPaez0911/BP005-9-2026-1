d2867513a14c07302db5527f6444247f05fc1217
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-05-27 04:36:19 -0500

    Update ejemplo5pag23.cpp

--- parcial2/1_Punteroscc++/funcionescyc++/ejemplo5pag23.cpp
+++ parcial2/1_Punteroscc++/funcionescyc++/ejemplo5pag23.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-// Prototipo de la función
+
 int potencia(int base, int exponente);
 
 int main() {
@@ -12,13 +12,13 @@ int main() {
 
     resultado = potencia(base, exponente);
     
-    // Se añadieron espacios entre las comillas para separar el texto de las variables
+
     cout << base << " elevado a " << exponente << " es " << resultado << endl;
 
     return 0;
 }
 
-// Definición de la función
+
 int potencia(int base, int exponente) {
     int resultado = 1;
     
