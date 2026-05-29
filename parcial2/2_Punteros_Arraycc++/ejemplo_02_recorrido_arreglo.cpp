f6e6d933391b035941c931a19cdff40403960837
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-05-29 17:10:20 -0500

    Create ejemplo_02_recorrido_arreglo.cpp

---
+++ parcial2/2_Punteros_Arraycc++/ejemplo_02_recorrido_arreglo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    /* Declaramos un arreglo de cinco enteros. */
+    int datos[5] = {8, 3, 15, 6, 10};
+
+    /* i será el índice usado para recorrer el arreglo. */
+    int i;
+
+    /* Recorremos los índices válidos: 0, 1, 2, 3 y 4. */
+    for (i = 0; i < 5; i++) {
+        /* En cada vuelta se muestra el elemento datos[i]. */
+        std::cout << "datos[" << i << "] = " << datos[i] << std::endl;
+    }
+
+    return 0;
+}
