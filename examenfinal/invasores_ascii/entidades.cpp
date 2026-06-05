4bb6f67affc339b8e8196dcd3227cc57b5becd48
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-06-05 14:18:02 -0500

    Update entidades.cpp

--- examenfinal/invasores_ascii/entidades.cpp
+++ examenfinal/invasores_ascii/entidades.cpp
@@ -13,18 +13,17 @@ void inicializarNave(Nave* n) {
 void moverNave(Nave* n, char dir) {
     if (n == nullptr) return;
     if (dir == 'a' && n->columna > 1) n->columna--; 
-    if (dir == 'd' && n->columna < COLUMNAS - 2) n->columna++; [span_19](start_span)[span_20](start_span)// Derecha con tope[span_19](end_span)[span_20](end_span)
-}
+    if (dir == 'd' && n->columna < COLUMNAS - 2) n->columna++;
 
 void inicializarBalas(Bala balas[], int tam) {
     for (int i = 0; i < tam; i++) {
-        balas[i].activa = false; [span_21](start_span)// Todas empiezan apagadas[span_21](end_span)
+        balas[i].activa = false; [span_21](start_span)
     }
 }
 
 void dispararBala(Bala balas[], int tam, Nave* n) {
     if (n == nullptr) return;
-    [span_22](start_span)// Buscamos la primera bala libre en el arreglo para activarla[span_22](end_span)
+    
     for (int i = 0; i < tam; i++) {
         if (!balas[i].activa) {
             balas[i].fila = n->fila - 1; // Sale arriba de la nave
@@ -64,12 +63,12 @@ void detectarColisiones(Bala balas[], int tamB, Enemigo enemigos[], int tamE, Ju
         for (int k = 0; k < tamE; k++) {
             if (!enemigos[k].vivo) continue;
             
-            [span_24](start_span)// Si coinciden en la misma coordenada[span_24](end_span)
+            [span_24](start_span)
             if (balas[i].fila == enemigos[k].fila && balas[i].columna == enemigos[k].columna) {
-                balas[i].activa = false; [span_25](start_span)// Desactiva bala[span_25](end_span)
-                enemigos[k].vivo = false; [span_26](start_span)// Mata enemigo[span_26](end_span)
-                j->puntaje += 10; [span_27](start_span)// Suma puntos[span_27](end_span)
-                j->enemigosDestruidos++; [span_28](start_span)// Cuenta la baja[span_28](end_span)
+                balas[i].activa = false;
+                enemigos[k].vivo = false; 
+                j->puntaje += 10;
+                j->enemigosDestruidos++;
             }
         }
     }
