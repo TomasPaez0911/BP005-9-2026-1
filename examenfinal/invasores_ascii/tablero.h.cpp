82e6afa3435f590709d0ad77908cfd997707ce87
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-06-04 23:12:48 -0500

    Add header file for game board functionality

---
+++ examenfinal/invasores_ascii/tablero.h.cpp
@@ -0,0 +1,12 @@
+#ifndef TABLERO_H
+#define TABLERO_H
+
+// Constantes para el tamaño del mapa
+const int FILAS = 15;
+const int COLUMNAS = 20;
+
+// Funciones para el tablero
+void limpiarTablero(char tablero[FILAS][COLUMNAS]);
+void mostrarTablero(char tablero[FILAS][COLUMNAS]);
+
+#endif
