2319887a190c4c87f208c96cdba449dbbf011bc7
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-06-06 19:31:17 -0500

    Update tablero.cpp

--- examenfinal/invasores_ascii/tablero.cpp
+++ examenfinal/invasores_ascii/tablero.cpp
@@ -4,7 +4,6 @@
 void limpiarTablero(char tablero[FILAS][COLUMNAS]) {
     for (int i = 0; i < FILAS; i++) {
         for (int j = 0; j < COLUMNAS; j++) {
-            
             if (j == 0 || j == COLUMNAS - 1) {
                 tablero[i][j] = '|';
             } else {
@@ -15,7 +14,6 @@ void limpiarTablero(char tablero[FILAS][COLUMNAS]) {
 }
 
 void mostrarTablero(char tablero[FILAS][COLUMNAS]) {
-    // Limpia la terminal en Linux antes de dibujar para que no se amontone
     std::system("clear"); 
     
     for (int i = 0; i < FILAS; i++) {
