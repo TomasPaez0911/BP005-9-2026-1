530a3d2e6a167f74182980bb007d06999e03b2d5
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-06-06 23:24:36 -0500

    Add entidades.h with game entity structures and functions

---
+++ examenfinalsinerrores/entidades.h
@@ -0,0 +1,27 @@
+#ifndef ENTIDADES_H
+#define ENTIDADES_H
+#include "juego.h" 
+struct Nave {
+    int fila;
+    int columna;
+    int vidas; 
+};
+struct Bala {
+    int fila;
+    int columna;
+    bool activa; 
+};
+struct Enemigo {
+    int fila;
+    int columna;
+    bool vivo; 
+};
+void inicializarNave(Nave* n);
+void moverNave(Nave* n, char dir);
+void inicializarBalas(Bala balas[], int tam);
+void dispararBala(Bala balas[], int tam, Nave* n);
+void actualizarBalas(Bala balas[], int tam);
+void inicializarEnemigos(Enemigo enemigos[], int tam);
+void detectarColisiones(Bala balas[], int tamB, Enemigo enemigos[], int tamE, Juego* j);
+
+#endif
