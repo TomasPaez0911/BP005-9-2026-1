9676d82041f6011d582e9bb2e914c89396825440
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-06-06 23:30:04 -0500

    Create tablero.h

---
+++ examenfinalsinerrores/tablero.h
@@ -0,0 +1,10 @@
+#ifndef TABLERO_H
+#define TABLERO_H
+
+const int FILAS = 20;    // O el número de filas que tenga tu mapa
+const int COLUMNAS = 40; // O el número de columnas que tenga tu mapa
+
+void limpiarTablero(char tablero[FILAS][COLUMNAS]);
+void mostrarTablero(char tablero[FILAS][COLUMNAS]);
+
+#endif
