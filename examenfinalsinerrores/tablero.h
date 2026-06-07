ec89b65022cc7aa5da188a388a8a6f671fe4aadd
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-06-07 00:51:03 -0500

    Change tablero dimensions to 10x20

--- examenfinalsinerrores/tablero.h
+++ examenfinalsinerrores/tablero.h
@@ -1,8 +1,8 @@
 #ifndef TABLERO_H
 #define TABLERO_H
 
-const int FILAS = 20;    // O el número de filas que tenga tu mapa
-const int COLUMNAS = 40; // O el número de columnas que tenga tu mapa
+const int FILAS = 10;    // O el número de filas que tenga tu mapa
+const int COLUMNAS = 20; // O el número de columnas que tenga tu mapa
 
 void limpiarTablero(char tablero[FILAS][COLUMNAS]);
 void mostrarTablero(char tablero[FILAS][COLUMNAS]);
