5ffea7fde1036417560e665ba356426c74d98356
TomasPaez0911 <tomasalejandropaezramirez@gmail.com>
2026-06-06 23:29:35 -0500

    Implement board initialization and display functions
    Added functions to initialize and display the game board.

---
+++ examenfinalsinerrores/tablero.cpp
@@ -0,0 +1,25 @@
+#include "tablero.h"
+#include <iostream>
+
+void limpiarTablero(char tablero[FILAS][COLUMNAS]) {
+    for (int i = 0; i < FILAS; i++) {
+        for (int j = 0; j < COLUMNAS; j++) {
+            if (j == 0 || j == COLUMNAS - 1) {
+                tablero[i][j] = '|';
+            } else {
+                tablero[i][j] = ' ';
+            }
+        }
+    }
+}
+
+void mostrarTablero(char tablero[FILAS][COLUMNAS]) {
+    std::system("cls"); 
+    
+    for (int i = 0; i < FILAS; i++) {
+        for (int j = 0; j < COLUMNAS; j++) {
+            std::cout << tablero[i][j];
+        }
+        std::cout << "\n";
+    }
+}
